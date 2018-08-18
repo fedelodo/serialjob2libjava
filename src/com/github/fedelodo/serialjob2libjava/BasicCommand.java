cc1e23865b3f6c2c29c533a7c67984f2304b8d30
federlodo <f.lodovici@outlook.com>
2018-08-18 16:35:28 +0200

    First Version

---
+++ src/com/github/fedelodo/serialjob2libjava/BasicCommand.java
@@ -0,0 +1,40 @@
+package com.github.fedelodo.serialjob2libjava;
+
+public class BasicCommand {
+    private final String type;
+    private final int index;
+    private final String argument;
+
+    public String getType() {
+        return type;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public String getArgument() {
+        return argument;
+    }
+
+    public BasicCommand(String type, int index, String argument) {
+        this.type = type;
+        this.index = index;
+        this.argument = argument;
+    }
+
+    public BasicCommand(String type, String argument) {
+        this.type = type;
+        this.argument = argument;
+        this.index = 0;
+    }
+
+    @Override
+    public String toString() {
+        if (type.equals("J")) {
+            return (char) 2 + type + argument + (char) 3;
+        } else {
+            return (char) 2 + type + String.format("%02d", index) + argument + (char) 3;
+        }
+    }
+}
---
+++ src/com/github/fedelodo/serialjob2libjava/Command.java
@@ -0,0 +1,40 @@
+package com.github.fedelodo.serialjob2libjava;
+
+public class Command {
+    private String command;
+    private BasicCommand basicCommand;
+    private SpecialCommand specialCommand;
+    private BasicCommand[] basicCommands = new BasicCommand[0];
+
+    Command(String command) {
+        this.command = command;
+    }
+
+    Command(BasicCommand basicCommand) {
+        this.basicCommand = basicCommand;
+    }
+
+    public Command(BasicCommand[] basicCommands) {
+        this.basicCommands = basicCommands;
+    }
+
+    Command(SpecialCommand specialCommand) {
+        this.specialCommand = specialCommand;
+    }
+
+    @Override
+    public String toString() {
+        if (basicCommand == null) {
+            return (char) 1 + command + (char) 4;
+        }
+        if (basicCommands.length > 0) {
+            StringBuilder result = new StringBuilder();
+            for (BasicCommand basicCommand1 : basicCommands) {
+                result.insert(0, (char) 1 + basicCommand1.toString() + (char) 4);
+            }
+            return result.toString();
+        } else {
+            return (char) 1 + basicCommand.toString() + (char) 4;
+        }
+    }
+}
