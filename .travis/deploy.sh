2d87bd70dd1e2f69f7cebc471d074341b901af3d
federlodo <f.lodovici@outlook.com>
2018-08-27 22:00:59 +0200

    Edited for CI

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 if [ ! -z "$TRAVIS_TAG" ]; then
     echo "on a tag -> set pom.xml <version> to $TRAVIS_TAG"
     mvn --settings .travis/.maven.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=$TRAVIS_TAG 1>/dev/null 2>/dev/null
